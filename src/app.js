47a704fb45850e5c204517cb74adc5b5a0c6fc2e
Arshad <attararshad004@gmail.com>
2026-01-15 21:14:54 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'OK' });
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
