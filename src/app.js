89a27bff7893525f4f4e12bde702879c7936bb0b
Arshad <attararshad004@gmail.com>
2026-01-16 21:14:15 +0530

    adding database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use('/test', (req, res) => {
+  res.send('Test route is working!');
 });
 
-app.get('/status', (req, res) => {
-  res.json({ status: 'OK' });
+app.get('/user', (req, res) => {
+  res.send({ firstName: 'John', lastName: 'Doe' });
 });
+
+app.post('/user', (req, res) => {
+  console.log('POST request received at /user');
+  res.send('Data received via POST request');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
