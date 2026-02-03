8451bee3c87e788021537af2fd1984897cede2df
Arshad <attararshad004@gmail.com>
2026-02-03 21:17:22 +0530

    ...

--- src/routes/request.js
+++ src/routes/request.js
@@ -51,10 +51,45 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     })
     
   }catch(err){
-    return res.status(400).send("ERROR" + err.message );
+    return res.status(400).send("ERROR:" + err.message );
   }
 
 
 });
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
+
+  try{
+    const loggedInUser = req.user;
+    const { requestId, status} = req.params;
+
+    const allowedStatus = ["accepted", "rejected"];
+    if(!allowedStatus.includes(status)){
+      return res
+      .status(400)
+      .json({message: "Invalid status type "+ status });
+    }
+    const connectionRequest = await Connection.findOne({
+      _id: requestId,
+      toUserId: loggedInUser._id,
+      status: "interested",
+    });
+    if (!connectionRequest) {
+      return res
+        .status(404)
+        .json({ message: "Connection request not found" });
+    }
+    connectionRequest.status = status;
+    const data = await connectionRequest.save();
+
+    res.json({
+      message: `Connection request has been ${status}ed.`,
+      data,
+    });
+
+  }catch(err){
+    return res.status(400).send("ERROR:" + err.message );
+  }
+
+});
 module.exports = requestRouter;
